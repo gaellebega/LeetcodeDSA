9503f102f9755bb4bc2ad979500d9119cd62bdec
gaellebega <gaellebega@gmail.com>
2026-03-18 00:51:44 +0200

    longest substring eithout reperating characters

--- 3.longestsubstringwithoutrepeatingcharacter.py
+++ 3.longestsubstringwithoutrepeatingcharacter.py
@@ -1,15 +1,16 @@
 def lengthOfLongestSubstring(self, s: str) -> int:
   right=0
-  left=1
-  new=()
-  while(left<right):
-    if s[left]!=s[right]:
-       new.add(s[left])
-       new.add(s[right])
-       left+=1
-     return len(new)  
-    elif s[left]==s[right]:
-        return 1
-
-
+  left=0
+  new=set()
+  max_len=0
+  while(right<len(s)):
+    if s[left] not in new:
+      s.add(s[left])
+      right+=1
+    else:
+      s.remove(s[right])
+      left-=1
+      max_len(max_len,len(s))
+  return max_len    
+   
 
