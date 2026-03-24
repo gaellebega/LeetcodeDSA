1082c189718b41b92a894c335d59dc3c4f091f59
gaellebega <gaellebega@gmail.com>
2026-03-24 17:09:32 +0200

    in process merging sort

--- 88.mergesortedarray.py
+++ 88.mergesortedarray.py
@@ -1,4 +1,8 @@
 def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) :
   # the logic to make it cretae the empty elements then choose from nums1 and nums 2 the element which are the smallest then put it in the merged array then continue the process till the end
 
+  # another logic to go in the nums1 array then we go to the ened then we put on the right pointer then we chosee the largest num in the nums2 and then we put it there
+  # compare the last in the both arrays
+
+  last_index=m+n-1
   
