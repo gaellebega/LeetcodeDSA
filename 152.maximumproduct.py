4d7ac3eb06f3ee6636377a3d179b3a7edfe10c06
gaellebega <gaellebega@gmail.com>
2026-04-10 23:47:02 +0200

    maximumproduct

---
+++ 152.maximumproduct.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if len(nums)<2:
+            return 0
+        maximum=nums[0]*nums[1]   
+        for i in range(1,len(nums)-1):
+            product=nums[i]*nums[i+1]
+            maximum=max(product,maximum)
+        return maximum    
+        
